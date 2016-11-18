bede0040b551a3e08c9c935c8ac0b9099b6367c5
Alessio Garzi <gun101@email.it>
2016-11-18 16:18:42 +0100

    Update mysqluploader.sh

--- mysqluploader.sh
+++ mysqluploader.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+
+if [ -z "$TARGETDIR" ]; then
+    echo "Targetdir env var not found"
+    exit 1
+fi
+
 for target in $(grep '^\[.*\]$' /root/.rclone.conf  | cut -c 2- | rev | cut -c 2- | rev); do
 	echo 'rclone sync /var/lib/automysqlbackup $target:/automysqlbackup/'
-	rclone sync /var/lib/automysqlbackup $target:/automysqlbackup/
+	rclone sync /var/lib/automysqlbackup $target:/$TARGETDIR
 done
